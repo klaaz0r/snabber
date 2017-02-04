d8329dd437de90db16003498d658487d8580d151
klaaz0r <klaas.foppen@gmail.com>
2017-02-04 16:45:13 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import { h } from 'snabbdom/h'
+import { head, take, omit } from 'ramda'
+
+function isValidString(param) {
+  return typeof param === 'string' && param.length > 0
+}
+
+function isSelector(param) {
+  return isValidString(param) && (param[0] === '.' || param[0] === '#')
+}
+
+function createTag(tagName) {
+  return function helper(selector, b, c) {
+    if (isSelector(selector)) {
+      if (typeof b !== 'undefined' && typeof c !== 'undefined') {
+        return h(tagName + selector, b, c)
+      } else if (typeof b !== 'undefined') {
+        return h(tagName + selector, b)
+      } else {
+        return h(tagName + selector, {})
+      }
+    } else if (!!b) {
+      return h(tagName, selector, b)
+    } else if (!!selector) {
+      return h(tagName, selector)
+    } else {
+      return h(tagName, {})
+    }
+  }
+}
+
+const TAG_NAMES = [
+  'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base',
+  'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption',
+  'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'dir', 'div', 'dl',
+  'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'footer', 'form',
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html',
+  'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend',
+  'li', 'link', 'main', 'map', 'mark', 'menu', 'meta', 'nav', 'noscript',
+  'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'progress', 'q',
+  'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small',
+  'source', 'span', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td',
+  'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'u', 'ul', 'video',
+]
+
+const exported = { TAG_NAMES, createTag }
+
+TAG_NAMES.forEach(n => { exported[n] = createTag(n) })
+
+export default exported
---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import { h1, div, span, a } from './index'
+import toHTML from 'snabbdom-to-html'
+import h from 'snabbdom/h'
+import { expect } from 'chai'
+
+describe('test snabbdom helpers', function() {
+  describe('all', function() {
+    it('should parse singel item', function() {
+      const node = h1('.header', { style: { background: 'red' } }, 'hello world')
+      const html = `<h1 class="header" style="background: red">hello world</h1>`
+      expect(toHTML(node)).to.equal(html)
+    })
+    it('should parse nested items', function() {
+      const node = div('#container.two.classes', { style: { background: 'red' } }, [
+        span('.box', { style: { fontWeight: 'bold' } }, 'This is bold'),
+        a('I\'ll take you places!')
+      ])
+      const html = `<div id="container" class="two classes" style="background: red"><span class="box" style="font-weight: bold">This is bold</span><a>I'll take you places!</a></div>`
+      expect(toHTML(node)).to.equal(html)
+    })
+  })
+})
